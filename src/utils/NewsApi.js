ddbd6c6096d17c4ce29b4dcb72b23572429311f8
Igor Yakunin <63667835+Nexxer@users.noreply.github.com>
2021-01-28 12:24:58 +0300

    settings newsAPI

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -2,11 +2,9 @@ export const getArticlesFromNewsApi = (valueToSearch) => {
   const dateFrom = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString();
   const dateTo = new Date(Date.now()).toISOString();
   const apiKey = 'cf8f4dfa14654e469bf7dfbbe087ab76';
-  // const apiKey = '1a6b6b1b0d184383811540dafeadc7ac';
-  // const apiKey = 'aeb6d1408e304fc385558c5bbe2b0e45';
 
   return fetch(
-    `https://newsapi.org/v2/everything?q=${valueToSearch}&from=${dateFrom}&to=${dateTo}&pageSize=${'7'}&apiKey=${apiKey}`,
+    `https://newsapi.org/v2/everything?q=${valueToSearch}&from=${dateFrom}&to=${dateTo}&pageSize=${'100'}&apiKey=${apiKey}`,
     {
       method: 'GET',
     },
